5561801cf5dc579067f551f00bbb46349980a8ae
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-08-10 23:22:38 +0900

    netfilx 업데이트

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="react_netflix">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
